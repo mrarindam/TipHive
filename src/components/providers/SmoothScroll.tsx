6e5dd8a47f42a3315349969a0b709430b2a36e41
mrarindam <marindam342@gmail.com>
2026-05-19 23:07:12 +0530

    scroll fixes

--- src/components/providers/SmoothScroll.tsx
+++ src/components/providers/SmoothScroll.tsx
@@ -1,28 +1,7 @@
 'use client';
 
-import { ReactLenis, useLenis } from 'lenis/react';
-import { ReactNode, useEffect } from 'react';
-import { usePathname } from 'next/navigation';
+import { ReactNode } from 'react';
 
 export default function SmoothScroll({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
-  const lenis = useLenis();
-
-  useEffect(() => {
-    if (lenis) {
-      lenis.scrollTo(0, { immediate: true });
-    }
-  }, [pathname, lenis]);
-
-  return (
-    <ReactLenis root options={{
-      lerp: 0.05,
-      duration: 1.5,
-      smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
-    }}>
-      {children}
-    </ReactLenis>
-  );
+  return <>{children}</>;
 }
